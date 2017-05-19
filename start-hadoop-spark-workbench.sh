a20c013d389d115ab937a3bfde357a51d639a055
Bartłomiej Tomala <bartholomaios@gmail.com>
2017-05-19 16:51:48 +0200

    mend

--- start-hadoop-spark-workbench.sh
+++ start-hadoop-spark-workbench.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker-compose up -d
+docker-compose up -d namenode
+docker-compose up -d datanode
+docker-compose up -d spark-master spark-worker spark-notebook hue
 
 my_ip=`ip route get 1|awk '{print $NF;exit}'`
 echo "Namenode: http://${my_ip}:50070"
@@ -8,4 +10,3 @@ echo "Datanode: http://${my_ip}:50075"
 echo "Spark-master: http://${my_ip}:8080"
 echo "Spark-notebook: http://${my_ip}:9001"
 echo "Hue (HDFS Filebrowser): http://${my_ip}:8088/home"
-
